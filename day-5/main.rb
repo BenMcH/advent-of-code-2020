0e2b582d83c43d13caa7870869c0980182575504
Ben McHone <benjamin.mchone@gmail.com>
2020-12-05 07:50:46 -0600

    Advent of code day 5 final set of golfing

--- day-5/main.rb
+++ day-5/main.rb
@@ -13,15 +13,10 @@ def get_pos(max, instructions, upper_char)
     ids[0]
 end
 
-def get_seat_id(row, col)
-    row * 8 + col
-end
+all_seat_ids = boarding_passes.map{|pass| get_pos(127, pass[0], 'F') * 8 + get_pos(7, pass[1], 'L')}
+puts all_seat_ids.max
 
-all_seat_ids = boarding_passes.map{|pass| get_seat_id(get_pos(127, pass[0], 'F'), get_pos(7, pass[1], 'L'))}
-max_seat_id = all_seat_ids.max
-puts max_seat_id
-
-missing_seat = max_seat_id
+missing_seat = all_seat_ids.max
 missing_seat = missing_seat - 1 while all_seat_ids.include?(missing_seat)
 
 puts missing_seat
