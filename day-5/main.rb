b0805a5dcb74c2241a429f01aaa51ba95dfdb909
Ben McHone <benjamin.mchone@gmail.com>
2020-12-05 08:06:31 -0600

    Advent of code day 5; Final final final golf

--- day-5/main.rb
+++ day-5/main.rb
@@ -6,8 +6,7 @@ end
 
 boarding_passes = File.read('./data.txt').split.map{|pass|[pass[0..6], pass[7..-1]]}
 all_seat_ids = boarding_passes.map{|pass| get_pos(127, pass[0], 'F') * 8 + get_pos(7, pass[1], 'L')}
-missing_seat = all_seat_ids.max
-missing_seat = missing_seat - 1 while all_seat_ids.include?(missing_seat)
+missing_seat = all_seat_ids.sort.find{|pass| !all_seat_ids.include?(pass+1)} + 1
 
 puts all_seat_ids.max
 puts missing_seat
