f2c1e97d421cb4526479daf1c1fc11ec1e8823fc
Ben McHone <benjamin.mchone@gmail.com>
2020-12-09 06:51:59 -0600

    Advent of Code 2020 Day 9 Cleanup

--- day-9/main.rb
+++ day-9/main.rb
@@ -1,40 +1,21 @@
 numbers = File.read('./data.txt').split("\n").map(&:to_i)
 
-target = -1
+target = numbers[25..-1].find.with_index do |number, index|
+    preamble = numbers[index..index+24]
+    sums = preamble.product(preamble).map{|set| set.sum}
 
-numbers[25..-1].find.with_index do |number, index|
-    preamble = numbers[index..(index+24)]
-    sums = preamble.product(preamble).map{|set| set.reduce(&:+)}
-
-    if sums.any?{|num| num == number}
-        next
-    end
-    target = number
-    break
+    sums.all?{|num| num != number}
 end
 
 puts target
 
-start_index = 0
-length = 0
+numbers.each.with_index do |_, index|
+    numbers[index+1..-1].each.with_index do |_, another_index|
+        range = index..index+another_index + 1
 
-numbers.each.with_index do |number, index|
-    start_index = index
-    sum = number
-
-    numbers[index+1..-1].each.with_index do |another_one, another_index|
-        length = another_index + 1
-        sum += another_one
-
-        if sum >= target
+        if numbers[range].sum == target
+            puts numbers[range].minmax.sum if numbers[range].sum == target
             break
         end
     end
-    if sum == target
-        break
-    end
 end
-
-arr = numbers[start_index..start_index+length]
-
-puts arr.min + arr.max
