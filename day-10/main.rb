1802bb129ffdac060ebc04e4455ddb298d903af3
Ben McHone <benjamin.mchone@gmail.com>
2020-12-10 06:29:45 -0600

    Advent of Code 2020 Day 10 Part 1 Refactor

--- day-10/main.rb
+++ day-10/main.rb
@@ -1,13 +1,6 @@
 jolts = File.read('./data.txt').split("\n").map(&:to_i).sort
 
-differences = jolts.map.with_index do |jolt, index|
-    if index == 0
-        jolt
-    else
-        jolt - jolts[index-1]
-    end
-end
-
+differences = jolts.map.with_index { |jolt, index| index == 0 ? jolt : jolt - jolts[index-1] }
 one_jolt_differences = differences.select{|diff| diff == 1}.length
 three_jolt_differences = differences.select{|diff| diff == 3}.length + 1 # For your device
 
