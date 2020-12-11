c3f2eb78d7143d625e079c3c09910c7007f60b28
Ben McHone <benjamin.mchone@gmail.com>
2020-12-10 20:07:06 -0600

    Advent of Code Day 10 Super Golfing

--- day-10/main.rb
+++ day-10/main.rb
@@ -6,21 +6,16 @@ three_jolt_differences = differences.count{|diff| diff == 3} + 1 # For your devi
 
 p one_jolt_differences * three_jolt_differences
 
-graph = {}
-
 def fill_graph(parent, graph, jolts)
-    unless graph[parent]
-        graph[parent] = jolts.select{|j| (1..3).include?(j - parent)}
-        graph[parent].each{|jolt| fill_graph(jolt, graph, jolts.select{|j| j > jolt}) }
-    end
+    (graph[parent] = jolts.select{|j| (1..3).include?(j - parent)}).each{|jolt| fill_graph(jolt, graph, jolts.select{|j| j > jolt}) } unless graph[parent]
 end
-
-fill_graph(0, graph, jolts)
 
 def count_possibilities(graph_a, number, memo = {})
     memo[number] ||= number == graph_a[-1][0] ? 1 : graph_a.find{|a| a[0] == number}[1].map{|a| count_possibilities(graph_a, a, memo)}.sum
 end
 
+graph = {}
+fill_graph(0, graph, jolts)
 p count_possibilities(graph.to_a, 0)
 
 
