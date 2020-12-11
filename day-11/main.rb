a724b3e23b0f9c78b696d67e44e5ab982cbc018a
Ben McHone <benjamin.mchone@gmail.com>
2020-12-11 06:45:27 -0600

    Advent of Code Day 11 Refactors

--- day-11/main.rb
+++ day-11/main.rb
@@ -6,24 +6,15 @@ $states = {
     TAKEN: '#'
 }
 
-$directions = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1],
-]
+$directions = [ [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1] ]
 
 def count_neighbors(game_board, y, x)
     $directions.map do |direction|
         new_y, new_x = y + direction[0], x + direction[1]
-        if new_y < 0 || new_y >= game_board.length || new_x < 0 || new_x >= game_board[y].length
+        if !(0..game_board.length - 1).include?(new_y) || !(0..game_board[new_y].length - 1).include?(new_x)
             0
         else
-            game_board[new_y][new_x] == '#' ? 1 : 0
+            game_board[new_y][new_x] == $states[:TAKEN] ? 1 : 0
         end
     end.sum
 end
@@ -32,14 +23,13 @@ board = seats.dup
 while true
     new_board = board.map.with_index do |line, y|
         line.map.with_index do |seat, x|
-            neighbors = count_neighbors(board, y, x)
 
             if seat == $states[:FLOOR]
                 seat
             elsif seat == $states[:EMPTY]
-                neighbors == 0 ? $states[:TAKEN] : seat
+                count_neighbors(board, y, x) == 0 ? $states[:TAKEN] : seat
             else
-                neighbors >= 4 ? $states[:EMPTY] : seat
+                count_neighbors(board, y, x) >= 4 ? $states[:EMPTY] : seat
             end
         end
     end
@@ -52,9 +42,10 @@ p board.map {|row| row.count{|seat| seat == $states[:TAKEN] }}.sum
 
 def count_far_neighbors(game_board, y, x)
     $directions.map do |direction|
+        y_range = (0..game_board.length - 1)
         new_y, new_x = y + direction[0], x + direction[1]
-        new_y, new_x = new_y + direction[0], new_x + direction[1] while (0..game_board.length - 1).include?(new_y) && (0..game_board[new_y].length - 1).include?(new_x) && game_board[new_y][new_x] == $states[:FLOOR]
-        if new_y < 0 || new_y >= game_board.length || new_x < 0 || new_x >= game_board[y].length
+        new_y, new_x = new_y + direction[0], new_x + direction[1] while y_range.include?(new_y) && (0..game_board[new_y].length - 1).include?(new_x) && game_board[new_y][new_x] == $states[:FLOOR]
+        if !y_range.include?(new_y) || new_x < 0 || new_x >= game_board[y].length
             0
         else
             game_board[new_y][new_x] == '#' ? 1 : 0
@@ -66,14 +57,12 @@ board = seats.dup
 while true
     new_board = board.map.with_index do |line, y|
         line.map.with_index do |seat, x|
-            neighbors = count_far_neighbors(board, y, x)
-
-            if seat == $states[:FLOOR]
-                seat
-            elsif seat == $states[:EMPTY]
-                neighbors == 0 ? $states[:TAKEN] : seat
+            if seat == $states[:EMPTY]
+                count_far_neighbors(board, y, x) == 0 ? $states[:TAKEN] : seat
+            elsif seat == $states[:TAKEN]
+                count_far_neighbors(board, y, x) >= 5 ? $states[:EMPTY] : seat
             else
-                neighbors >= 5 ? $states[:EMPTY] : seat
+                seat
             end
         end
     end
