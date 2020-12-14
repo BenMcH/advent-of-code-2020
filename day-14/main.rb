2fccf784431514dccb7f034a82809a5dfb131e72
Ben McHone <benjamin.mchone@gmail.com>
2020-12-14 16:53:31 -0600

    Advent of Code 2020 Day 14 Part 2

--- day-14/main.rb
+++ day-14/main.rb
@@ -18,10 +18,44 @@ def init(sets)
             memory[index] = value.to_i(2)
         end
     end
-    
+
     memory
 end
 
-memory = init(sets)
+p init(sets).values.sum
 
-p memory.values.sum
+def create_indexes(mask, index)
+    pairs = mask.chars.map.with_index{|c, i| [c, i]}.select{|c| c[0] == 'X'}
+    start, *others = mask.chars.map.with_index do |m, i|
+        if m == '1'
+            [1]
+        elsif m == 'X'
+            [0, 1]
+        else
+            [index[i]]
+        end
+    end
+
+    start.product(*others).map(&:join)
+end
+
+def init_part_2(sets)
+    memory = {}
+    sets.each do |set|
+        _mask, *rules = set
+        mask = _mask[7..-1]
+
+        rules.each do |rule|
+            index, value = /mem\[(\d+)\] = (\d+)/.match(rule).captures
+            index, value = index.to_i.to_s(2).rjust(36, '0'), value.to_i
+
+            create_indexes(mask, index).each do |index|
+                memory[index.to_i(2)] = value
+            end
+        end
+    end
+
+    memory
+end
+
+p init_part_2(sets).values.sum
