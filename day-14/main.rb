43bba866d6a4db95e4ed2dd7e587b496df4bc037
Ben McHone <benjamin.mchone@gmail.com>
2020-12-14 17:02:18 -0600

    Advent of Code 2020 Day 14 :golf:

--- day-14/main.rb
+++ day-14/main.rb
@@ -1,19 +1,15 @@
-sets = File.read("./data.txt").split("mask").select{|line| line.include?("\n")}.map{|set| "mask#{set}".split("\n")}
+sets = File.read("./data.txt").split("mask = ").map{|set| "#{set}".split("\n")}
 
 def init(sets)
     memory = {}
     sets.each do |set|
         mask, *rules = set
-        mask = mask[7..-1]
 
         rules.each do |rule|
             index, value = /mem\[(\d+)\] = (\d+)/.match(rule).captures
             index, value = index.to_i, value.to_i.to_s(2).rjust(36, '0')
 
-            mask.chars.each.with_index do |m, idx|
-                next if m == 'X'
-                value[idx] = m
-            end
+            mask.chars.each.with_index { |m, idx| value[idx] = m unless m == 'X' }
 
             memory[index] = value.to_i(2)
         end
@@ -25,33 +21,20 @@ end
 p init(sets).values.sum
 
 def create_indexes(mask, index)
-    pairs = mask.chars.map.with_index{|c, i| [c, i]}.select{|c| c[0] == 'X'}
-    start, *others = mask.chars.map.with_index do |m, i|
-        if m == '1'
-            [1]
-        elsif m == 'X'
-            [0, 1]
-        else
-            [index[i]]
-        end
-    end
-
+    start, *others = mask.chars.map.with_index { |m, i| m == '1' ? [1] : m == 'X' ? [0, 1] : [index[i]] }
     start.product(*others).map(&:join)
 end
 
 def init_part_2(sets)
     memory = {}
     sets.each do |set|
-        _mask, *rules = set
-        mask = _mask[7..-1]
+        mask, *rules = set
 
         rules.each do |rule|
             index, value = /mem\[(\d+)\] = (\d+)/.match(rule).captures
             index, value = index.to_i.to_s(2).rjust(36, '0'), value.to_i
 
-            create_indexes(mask, index).each do |index|
-                memory[index.to_i(2)] = value
-            end
+            create_indexes(mask, index).each { |index| memory[index.to_i(2)] = value }
         end
     end
 
