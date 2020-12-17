9f5b796c525a371e0f1354f3d7a723b5b54fe3d1
Ben McHone <benjamin.mchone@gmail.com>
2020-12-16 21:25:46 -0600

    Advent of Code Day 16 :golf: cleanup

--- day-16/main.rb
+++ day-16/main.rb
@@ -11,38 +11,26 @@ criteria.split("\n").each do |crt|
     rules << [(x1.to_i..x2.to_i), (y1.to_i..y2.to_i)]
 end
 
-nearby_tickets = nearby_tickets.map do |ticket|
-    ticket.split(',').map(&:to_i)
-end
-
-flat_rules = rules.flatten
-invalid_fields = nearby_tickets.flatten.select{|field| flat_rules.none?{|rule| rule.include? field }}
+nearby_tickets = nearby_tickets.map { |ticket| ticket.split(',').map(&:to_i) }
+invalid_fields = nearby_tickets.flatten.select{|field| rules.flatten.none?{|rule| rule.include? field }}
 
 p invalid_fields.sum
 
 valid_tickets = nearby_tickets.select{|ticket| ticket.none?{|i| invalid_fields.include? i}}
 
-columns = valid_tickets.transpose
-
-column_rule_mapping = columns.map do |col|
+column_rule_mapping = valid_tickets.transpose.map do |col|
     rules.select{|rule| col.all?{|v| rule.any?{|r| r.include? v}}}.map{|rule| rules.index(rule)}
 end
 
-
 until column_rule_mapping.none?{|c| c.kind_of?(Array)}
     r = column_rule_mapping.find{|a| a.kind_of?(Array) && a.length == 1}
-    i = column_rule_mapping.index(r)
-    column_rule_mapping[i] = r[0]
+    column_rule_mapping[column_rule_mapping.index(r)] = r[0]
     column_rule_mapping.each.with_index do |rule, idx|
-        if rule.kind_of?(Array)
-            column_rule_mapping[idx] = column_rule_mapping[idx].select{|i| i != r[0]}
-        end
+        column_rule_mapping[idx] = column_rule_mapping[idx].select{|i| i != r[0]} if rule.kind_of? Array
     end
 end
 
-map = column_rule_mapping.map.with_index do |x, i|
-    [names[x], i]
-end
+map = column_rule_mapping.map.with_index { |x, i| [names[x], i] }
 
 p map.select{|n| n[0].start_with? "depart"}.map{|name| my_ticket[name[1]]}.reduce(&:*)
 
