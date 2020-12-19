94e203c1c1188a55a8352d03970f89f86644f28b
Ben McHone <benjamin.mchone@gmail.com>
2020-12-18 21:21:51 -0600

    Day 18 :golf:

--- day-18/main.rb
+++ day-18/main.rb
@@ -4,18 +4,10 @@ class Integer
     def -(operator)
         self * operator
     end
-end
-
-p problems.map{ |prob| eval(prob.gsub('*', '-')) }.sum
-
-class Integer
-    def -(operator)
-        self * operator
-    end
-
     def /(operator)
         self + operator
     end
 end
 
+p problems.map{ |prob| eval(prob.gsub('*', '-')) }.sum
 p problems.map{ |prob| eval(prob.gsub('*', '-').gsub('+', '/')) }.sum
