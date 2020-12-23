e0457160460a7189c0958a01dacf19e3f04dd6ad
Ben McHone <benjamin.mchone@gmail.com>
2020-12-22 20:12:51 -0600

    Advent of Code 2020 Day 22 Part 2

--- day-22/main.rb
+++ day-22/main.rb
@@ -19,3 +19,38 @@ end
 hand = h1.length > 0 ? h1 : h2
 
 p hand.reverse.map.with_index{|n, i| n * (i + 1)}.sum
+
+def recursive_combat(h1, h2)
+    cache = []
+    until h1.empty? || h2.empty?
+        hash = [h1, h2].hash
+        if cache.include? hash
+            return [1, h1]
+        end
+        cache << hash
+
+        p1 = h1.shift
+        p2 = h2.shift
+
+        if p1 <= h1.size && p2 <= h2.size
+            winner = recursive_combat(h1.take(p1), h2.take(p2))[0]
+
+            if winner == 1
+                h1 = [*h1, p1, p2]
+            else
+                h2 = [*h2, p2, p1]
+            end
+        else
+            if p1 > p2
+                h1 = [*h1, p1, p2]
+            else
+                h2 = [*h2, p2, p1]
+            end
+        end
+    end
+
+    return h2.empty? ? [1, h1] : [2, h2]
+end
+
+
+p recursive_combat(hand_one, hand_two)[1].reverse.map.with_index{|n, i| n * (i + 1)}.sum
