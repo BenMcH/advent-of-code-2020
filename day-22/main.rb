4a6698f2f30322720a94617902274633c76ddfe9
Ben McHone <benjamin.mchone@gmail.com>
2020-12-22 20:21:26 -0600

    Advent of Code Day 22 Part 2 :golf:

--- day-22/main.rb
+++ day-22/main.rb
@@ -6,7 +6,7 @@ h2 = hand_two.dup
 until h1.length == 0 || h2.length == 0
     p1, *p1_l = h1
     p2, *p2_l = h2
-    
+
     if p1 > p2
         h1 = [*p1_l, p1, p2]
         h2 = p2_l
@@ -24,29 +24,19 @@ def recursive_combat(h1, h2)
     cache = []
     until h1.empty? || h2.empty?
         hash = [h1, h2].hash
-        if cache.include? hash
-            return [1, h1]
-        end
+        return [1, h1] if cache.include? hash
         cache << hash
-
         p1 = h1.shift
         p2 = h2.shift
 
-        if p1 <= h1.size && p2 <= h2.size
-            winner = recursive_combat(h1.take(p1), h2.take(p2))[0]
-
-            if winner == 1
-                h1 = [*h1, p1, p2]
-            else
-                h2 = [*h2, p2, p1]
-            end
+        winner = if p1 <= h1.size && p2 <= h2.size
+            recursive_combat(h1.take(p1), h2.take(p2))[0]
         else
-            if p1 > p2
-                h1 = [*h1, p1, p2]
-            else
-                h2 = [*h2, p2, p1]
-            end
+            p1 > p2 ? 1 : 2
         end
+        a, b = winner == 1 ? [[p1, p2], []] : [[], [p2, p1]]
+        h1 = h1.concat(a)
+        h2 = h2.concat(b)
     end
 
     return h2.empty? ? [1, h1] : [2, h2]
